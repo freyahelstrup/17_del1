b5f5bab9123512aa9f5e16b7284d371e583cfc72
freya <freya@DESKTOP-V3OEOO7.eduroam.wireless.dtu.dk>
2016-10-03 21:16:18 +0200

    Dice class updated to match new specifications

--- CDIO_del1/src/diceGame/Dice.java
+++ CDIO_del1/src/diceGame/Dice.java
@@ -1,19 +1,29 @@
 package diceGame;
 
 public class Dice {
+	
+	private int value1, value2;
+	
 	// roll the die and return the value (1-6) 
-	public int roll() {
+	private int roll() {
 		float d1=(float)Math.random();     // 0-1
 		float d2=(float)(d1*6-0.5);        // -0.5 - 5.5
 		int d3=Math.round(d2);             // 0-5 integer
-		return d3 + 1;                     // 1-6		
+		return d3 + 1;                     // 1-6
 	}
 	
-	// roll the die n times and print the values
-	public void rollMultiple(int n) {
-		for (int i=1; i<=n; i++) {
-			int roll = roll();
-			System.out.print(roll + " ");
-		}		
+	// roll the die twice and save values in value1 and value2
+	public void rollDice(){
+		Dice dice;
+		value1 = dice.roll();
+		value2 = dice.roll();
+	}
+	
+	public int getValue1(){
+		return value1;
+	}
+	
+	public int getValue2(){
+		return value2;
 	}
 }
